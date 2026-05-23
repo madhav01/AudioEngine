df85406f73787547a7bdc0ec3b2b96daf5c47fb0
Madhav <singhmadhav828@gmail.com>
2026-05-23 22:52:32 +0530

    updated .h file

--- audioengine.h
+++ audioengine.h
@@ -8,10 +8,15 @@ class AudioEngine : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
+
+    Q_PROPERTY(int level READ level NOTIFY levelChanged FINAL)
 public:
     explicit AudioEngine(QObject *parent = nullptr);
-
+    int level()const;
 signals:
+    void levelChanged();
+private:
+    int m_level=0;
 };
 
 #endif // AUDIOENGINE_H
